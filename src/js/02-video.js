837de0d873c5a389ba8e3283d4aa13ccbc195669
Uliana Seredenko <uliana1331@gmail.com>
2023-11-15 14:18:05 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -8,3 +9,7 @@ const getCurrentTime = currentTime => {
   const seconds = currentTime.seconds;
   localStorage.setItem(localStorageKey, JSON.stringify(seconds));
 };
+
+player.on('timeupdate', throttle(getCurrentTime, 100));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(localStorageKey)) || 0);
