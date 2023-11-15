4857f1b2d727ef4812d5889391a8eed0b28a6d64
Uliana Seredenko <uliana1331@gmail.com>
2023-11-15 12:37:02 +0200

    update
    gallery done
    video player start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const listMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}"
+      alt="${description}" />
+   </a>
+</li>
+    `
+  )
+  .join('');
+
+console.log(listMarkup);
+
+galleryList.innerHTML = listMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
+
+const getCurrentTime = currentTime => {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(localStorageKey, JSON.stringify(seconds));
+};
