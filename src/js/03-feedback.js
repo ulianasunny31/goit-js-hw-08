972eaa14976dd7f5e8491df32bb2710d54d5152e
Uliana Seredenko <uliana1331@gmail.com>
2023-11-18 12:33:30 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,31 +5,43 @@ const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 const localStorageKey = 'feedback-form-state';
 
-const stateOfStorage = JSON.parse(localStorage.getItem(localStorageKey) || {});
-messageInput.value = stateOfStorage.message || '';
-emailInput.value = stateOfStorage.email || '';
+let data = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+reloadThePage();
 
 form.addEventListener('input', function () {
   saveToLocalStorage();
 });
 
+function reloadThePage() {
+  if (data) {
+    messageInput.value = data.message || '';
+    emailInput.value = data.email || '';
+  }
+}
+
 const handleSubmit = event => {
   event.preventDefault();
+  if (emailInput.value !== '' || messageInput.value !== '') {
+    console.log({ message: messageInput.value, email: emailInput.value });
+  }
 
-  console.log('Email: ' + emailInput.value);
-  console.log('Message: ' + messageInput.value);
+  if (emailInput.value === '' || messageInput.value === '') {
+    return alert('Please fill in all the fields of the form!');
+  }
 
   localStorage.removeItem(localStorageKey);
-
-  form.reset();
+  event.currentTarget.reset();
+  data = {};
 };
 
 form.addEventListener('submit', handleSubmit);
 
 const saveToLocalStorage = throttle(() => {
-  const data = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
+  if (emailInput.value !== '' || messageInput.value !== '') {
+    data = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+  }
   localStorage.setItem(localStorageKey, JSON.stringify(data));
 }, 500);
